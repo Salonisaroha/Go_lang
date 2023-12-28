d1e95856beb698902bbfa1cf7f07b30bf0cebcf7
Saloni Saroha <salonisaroha5@gmail.com>
2023-12-28 22:02:32 +0530

    some methods of slice

--- slices/slice.go
+++ slices/slice.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 
 	"sort"
@@ -33,4 +34,40 @@ func main() {
 
 	courses = append(courses[:index], courses[index+1:]...)
 	fmt.Println(courses)
+
+	arr1 := []int{1, 2, 3, 4, 5}
+	slc := arr1[0:4]
+	fmt.Println("value of arr1 are :- ", arr1)
+	fmt.Println("value of slc are :-", slc)
+	slc[0] = 100
+	slc[1] = 101
+	slc[2] = 102
+	slc[3] = 104
+	fmt.Println("value of arr1 are :-", arr1)
+	fmt.Println("value of slice are :-", slc)
+
+	// composite literals :- These are those literals in which elements are present along with the decalration of slices.
+	s1 := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(s1)
+
+	// sorting the element
+
+	element := []int{23, 87, 45, 11, 10, 65, 29, 101}
+	fmt.Println("element before sorting are :-", element)
+
+	sort.Ints(element)
+	fmt.Println("elements after soritng are :-", element)
+
+	//triming of slices
+	valuesOfSlice := []byte{'s', 'a', 'l', 'o', 'n', 'i'}
+	for i := 0; i < len(valuesOfSlice); i++ {
+		fmt.Println(valuesOfSlice[i])
+	}
+
+	fmt.Printf("original characters are :- %s", valuesOfSlice)
+
+	//triming
+	response := bytes.Trim(valuesOfSlice, "!s")
+	fmt.Printf("\n slice with new value are :- %s", response)
+
 }
