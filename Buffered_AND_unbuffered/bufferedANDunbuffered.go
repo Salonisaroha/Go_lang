2f18cffd30f4743a8f217bea6d0ad00ab6b2ea2d
Saloni Saroha <salonisaroha5@gmail.com>
2024-01-01 19:24:58 +0530

    Buff

--- Buffered_AND_unbuffered/bufferedANDunbuffered.go
+++ Buffered_AND_unbuffered/bufferedANDunbuffered.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// A channel with a single "arrow" pointing towards it (e.g. <-chan int) is a receive-only channel, and can only be used to receive values. A channel with a single "arrow" pointing away from it (e.g. chan<- int) is a send-only channel
 func main() {
 	bufferedChannel := make(chan string, 3)
 
