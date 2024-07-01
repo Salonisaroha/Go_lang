bc964a370e06937be521b96c090f18f3c04ac2bb
Saloni Saroha <salonisaroha5@gmail.com>
2024-07-01 21:42:16 +0530

    Init function

---
+++ init function/init.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("Welcome to init function")
+}
+func main() {
+	fmt.Println("Welcome to main function")
+
+}
+func init() {
+	fmt.Println("Welcome to go lang")
+}
